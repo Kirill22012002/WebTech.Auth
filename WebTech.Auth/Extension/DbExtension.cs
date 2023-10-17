12d021278f121835f7fb1738d9766deaa0387356
Kirill <Perepechkin_2002@mail.ru>
2023-10-17 14:16:03 +0300

    add remote db

--- WebTech.Auth/Extension/DbExtension.cs
+++ WebTech.Auth/Extension/DbExtension.cs
@@ -1,8 +1,8 @@
 ﻿using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
 using WebTech.Auth.Data.Context;
 using WebTech.Auth.Data.DataSeeder;
+using WebTech.Auth.ErrorHandler.CustomExceptions;
 
 namespace WebTech.Auth.Extension;
 
@@ -10,17 +10,21 @@ public static class DbExtension
 {
     public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
     {
-        if (services == null) /*throw new CriticalServerException(nameof(services));*/ throw new Exception();
+        if (services == null) throw new CriticalServerException(nameof(services));
 
-        var conStrBuilder = new NpgsqlConnectionStringBuilder(
-                    configuration.GetConnectionString("UsersDatabase"));
+        string connectionString;
 
-        conStrBuilder.Password = "password";
-
-        var connection = conStrBuilder.ConnectionString;
+        if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+        {
+            connectionString = GetConnectionString();
+        }
+        else
+        {
+            connectionString = configuration.GetConnectionString("UsersDatabase");
+        }
 
         services.AddDbContext<AccessDbContext>(options =>
-            options.UseNpgsql(connection));
+            options.UseNpgsql(connectionString));
     }
 
     public static void ApplyMigrations(this IApplicationBuilder app)
@@ -43,5 +47,21 @@ public static class DbExtension
         using var scope = app.ApplicationServices.CreateScope();
         var dbSeed = scope.ServiceProvider.GetService<IDataSeeder>();
         dbSeed?.SeedAsync().Wait();
+    }
+
+    public static string GetConnectionString()
+    {
+        var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+        var userPassSide = connectionUrl.Split("@")[0];
+        var hostSide = connectionUrl.Split("@")[1];
+
+        var user = userPassSide.Split(":")[0];
+        var password = userPassSide.Split(":")[1];
+        var host = hostSide.Split("/")[0];
+        var database = hostSide.Split("/")[1].Split("?")[0];
+
+        return $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
     }
 }
--- WebTech.Auth/Program.cs
+++ WebTech.Auth/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.IdentityModel.Logging;
-using Microsoft.OpenApi.Models;
 using WebTech.Auth;
 using WebTech.Auth.ErrorHandler;
 using WebTech.Auth.Extension;
@@ -22,39 +21,7 @@ builder.ConfigureIdentityServer();
 
 builder.Services.AddLocalApiAuthentication();
 
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebTech.Auth", Version = "v1" });
-    
-    c.EnableAnnotations();
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = @"Enter 'Bearer' [space] and your token",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                },
-                Scheme="oauth2",
-                Name="Bearer",
-                In=ParameterLocation.Header
-            },
-            new List<string>()
-        }
-    });
-});
+builder.AddSwaggerGenConfiguration();
 
 IdentityModelEventSource.ShowPII = true;
 
